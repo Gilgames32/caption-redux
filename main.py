2e9864b63194c2a1c0018ecb293347b3289c0f25
Kovács Bálint <balintkovacs11@gmail.com>
2024-03-29 22:40:41 +0100

    reorganize folders

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import pipeline
-from args import imgpath, captiontext
+from src.pipeline import caption
+from src.args import imgpath, captiontext
 
 if __name__ == '__main__':
-    outpath = pipeline.caption(imgpath, captiontext)
+    outpath = caption(imgpath, captiontext)
     print(f"\nFinished caption at {outpath}")
